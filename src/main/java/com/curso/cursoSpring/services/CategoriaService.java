a5f91ef4f3f20d56ecf2e68f318722c8a29ef9a9
Pedro <pedroivis@gmail.com>
2020-08-19 19:51:32 -0300

    banco ta pica

---
+++ src/main/java/com/curso/cursoSpring/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.curso.cursoSpring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.cursoSpring.domain.Categoria;
+import com.curso.cursoSpring.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null); 
+	}
+
+}
