ed1e01c23936723040ef1d23623ce8fbcb2e08c4
Pedro <pedroivis@gmail.com>
2020-08-19 20:01:18 -0300

    seila q q foi agora

--- src/main/java/com/curso/cursoSpring/CursoSpringApplication.java
+++ src/main/java/com/curso/cursoSpring/CursoSpringApplication.java
@@ -1,13 +1,35 @@
 package com.curso.cursoSpring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringApplication {
+import com.curso.cursoSpring.domain.Categoria;
+import com.curso.cursoSpring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Jogo do Bixo");
+		Categoria cat2 = new Categoria(null, "Cartel");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+	
+	
 
 }
