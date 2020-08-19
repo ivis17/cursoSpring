dded82a3f4349b55b7bad094c69dedfd1b5f79af
Pedro <pedroivis@gmail.com>
2020-08-19 19:03:37 -0300

    Primeira classe

--- src/main/java/com/curso/cursoSpring/resources/CategoriaResource.java
+++ src/main/java/com/curso/cursoSpring/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.curso.cursoSpring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.cursoSpring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST is working";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Puteiro");
+		Categoria cat2 = new Categoria(2, "Biqueira");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 	
