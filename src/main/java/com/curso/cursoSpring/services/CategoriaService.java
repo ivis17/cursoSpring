14e99d9f9a646febece43c0f306f5021497a3f39
Pedro <pedroivis@gmail.com>
2020-08-20 19:47:42 -0300

    exceptions

--- src/main/java/com/curso/cursoSpring/services/CategoriaService.java
+++ src/main/java/com/curso/cursoSpring/services/CategoriaService.java
@@ -7,17 +7,21 @@ import org.springframework.stereotype.Service;
 
 import com.curso.cursoSpring.domain.Categoria;
 import com.curso.cursoSpring.repositories.CategoriaRepository;
+import com.curso.cursoSpring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null); 
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
 
 }
