8abc39760f751a8be1b71a4358dfa6344cd46fa1
Pedro <pedroivis@gmail.com>
2020-08-20 19:02:08 -0300

    classe 2

--- src/main/java/com/curso/cursoSpring/CursoSpringApplication.java
+++ src/main/java/com/curso/cursoSpring/CursoSpringApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.cursoSpring.domain.Categoria;
+import com.curso.cursoSpring.domain.Produto;
 import com.curso.cursoSpring.repositories.CategoriaRepository;
+import com.curso.cursoSpring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -23,10 +27,22 @@ public class CursoSpringApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Categoria cat1 = new Categoria(null, "Jogo do Bixo");
-		Categoria cat2 = new Categoria(null, "Cartel");
+		Categoria cat1 = new Categoria(null, "Puteiro");
+		Categoria cat2 = new Categoria(null, "Biqueira");
+		
+		Produto p1 = new Produto(null, "Puta", 200.00);
+		Produto p2 = new Produto(null, "Pó", 20.00);
+		Produto p3 = new Produto(null, "Traveco", 200.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 	
