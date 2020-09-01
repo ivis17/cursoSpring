683f74d29cab794735a59feb8e3a63aa691aafb6
Pedro <pedroivis@gmail.com>
2020-09-01 19:38:34 -0300

    listando todas categorias com dto

--- src/main/java/com/curso/cursoSpring/services/CategoriaService.java
+++ src/main/java/com/curso/cursoSpring/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.curso.cursoSpring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,9 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
-
 		Optional<Categoria> obj = repo.findById(id);
-
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-
 	}
 
 	public Categoria insert(Categoria obj) {
@@ -43,6 +41,11 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
+	}
+
+	public List<Categoria> findAll() {
+
+		return repo.findAll();
 	}
 
 }
