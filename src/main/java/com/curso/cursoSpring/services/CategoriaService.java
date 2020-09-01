b4fe3edd4b8c1abddd3cdb63cc67f7d57a28250c
Pedro <pedroivis@gmail.com>
2020-09-01 19:27:36 -0300

    delete categoria

--- src/main/java/com/curso/cursoSpring/services/CategoriaService.java
+++ src/main/java/com/curso/cursoSpring/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.curso.cursoSpring.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.curso.cursoSpring.domain.Categoria;
 import com.curso.cursoSpring.repositories.CategoriaRepository;
+import com.curso.cursoSpring.services.exceptions.DataIntegrityException;
 import com.curso.cursoSpring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,15 +25,24 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 
 }
