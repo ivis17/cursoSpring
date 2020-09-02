d27364d1f549101a3d5899cebdfce120b54ff802
Pedro <pedroivis@gmail.com>
2020-09-02 19:08:54 -0300

    validacao categoria

--- src/main/java/com/curso/cursoSpring/dto/CategoriaDTO.java
+++ src/main/java/com/curso/cursoSpring/dto/CategoriaDTO.java
@@ -4,16 +4,23 @@ import java.io.Serializable;
 
 import com.curso.cursoSpring.domain.Categoria;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
 
 	}
-	
+
 	public CategoriaDTO(Categoria obj) {
 		id = obj.getId();
 		nome = obj.getNome();
--- src/main/java/com/curso/cursoSpring/services/CategoriaService.java
+++ src/main/java/com/curso/cursoSpring/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.curso.cursoSpring.domain.Categoria;
+import com.curso.cursoSpring.dto.CategoriaDTO;
 import com.curso.cursoSpring.repositories.CategoriaRepository;
 import com.curso.cursoSpring.services.exceptions.DataIntegrityException;
 import com.curso.cursoSpring.services.exceptions.ObjectNotFoundException;
@@ -50,11 +51,15 @@ public class CategoriaService {
 
 		return repo.findAll();
 	}
-	
+
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		
-		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction),orderBy);
-		return  repo.findAll(pageRequest);
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
