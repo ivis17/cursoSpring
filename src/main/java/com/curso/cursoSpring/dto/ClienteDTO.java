3cebbe5b767d1d599637c2b6f71c2c3c73e05a24
Pedro <pedroivis@gmail.com>
2020-09-14 18:55:37 -0300

    customização validacao dnv

--- src/main/java/com/curso/cursoSpring/dto/ClienteDTO.java
+++ src/main/java/com/curso/cursoSpring/dto/ClienteDTO.java
@@ -8,7 +8,10 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.curso.cursoSpring.domain.Cliente;
+import com.curso.cursoSpring.services.validation.ClienteUpdate;
 
+
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
