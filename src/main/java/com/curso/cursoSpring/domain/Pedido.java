f37f9e64e316204a324c81aeb68ee7b3bd47b40b
Pedro <pedroivis@gmail.com>
2020-09-15 19:07:47 -0300

    total sub total

--- src/main/java/com/curso/cursoSpring/domain/Pedido.java
+++ src/main/java/com/curso/cursoSpring/domain/Pedido.java
@@ -39,7 +39,7 @@ public class Pedido implements Serializable {
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 
-	@OneToMany(mappedBy="id.pedido")
+	@OneToMany(mappedBy = "id.pedido")
 	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Pedido() {
@@ -52,6 +52,14 @@ public class Pedido implements Serializable {
 		this.instante = instante;
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
 	}
 
 	public Integer getId() {
